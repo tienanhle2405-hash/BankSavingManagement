9608adc6362cced1ce2070e16688cfe36e8387d1
tienanhle2405-hash <tienanhle2405@gmail.com>
2026-05-14 11:39:44 +0700

    Thay đổi

--- obj/Debug/net8.0/BankSavingManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/BankSavingManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankSavingManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b64c5386d1a14b835829e31d5758137163dad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11770c81e0284f1b3e407318059414b073c8c9f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankSavingManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankSavingManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
